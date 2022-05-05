a3fee17ecef43fcd65eac1df9ee83467bb3c223a
Belakhvostau Aliaksandr <aliaksandr@wsa-089-23b.local>
2022-05-05 09:26:10 +0300

    changed obj to config, format on save

--- script.js
+++ script.js
@@ -9,71 +9,74 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const createEl = (obj) => {
-    const tag = document.createElement(obj.el);
-    tag.textContent = obj.text;
-    tag.style.cssText = obj.styles;
+  const createEl = (config) => {
+    const tag = document.createElement(config.el);
+    tag.textContent = config.text;
+    tag.style.cssText = config.styles;
     return tag;
   };
 
   const createTable = async (tableCaption, thNames, fetchedData) => {
-      const table = createEl({
-        el: "table",
-        styles: `
+    const table = createEl({
+      el: "table",
+      styles: `
         width: 1200px;
         background: gray;
         padding: 20px;
         margin: 0 auto;
         text-align: center;
         border: 2px solid black;
-        `
-      }
-      );
+        `,
+    });
 
-      const caption = createEl({
-        el: "caption",
-        text: tableCaption,
-        styles:`
+    const caption = createEl({
+      el: "caption",
+      text: tableCaption,
+      styles: `
         color: gray;
         font-size: 30px;
         padding: 10px 0px;
-        `
-      });
-      const thead = createEl({el: "thead"});
-      const trHead = createEl({el: "tr"});
-      const tbody = createEl({el: "tbody"});
+        `,
+    });
+    const thead = createEl({ el: "thead" });
+    const trHead = createEl({ el: "tr" });
+    const tbody = createEl({ el: "tbody" });
 
-      table.append(caption);
-      table.append(thead);
-      table.append(tbody);
-      thead.append(trHead);
+    table.append(caption);
+    table.append(thead);
+    table.append(tbody);
+    thead.append(trHead);
 
-      thNames.forEach((title) => {
-        const th = createEl({
-          el:"th",
-          text: title,
-          styles:`
+    thNames.forEach((title) => {
+      const th = createEl({
+        el: "th",
+        text: title,
+        styles: `
           font-size: 18px;
           text-transform: uppercase;
           padding: 10px 0px;
-          `
-        });
-        trHead.append(th);
+          `,
       });
+      trHead.append(th);
+    });
 
-      const data = await fetchedData //!!!
+    const data = await fetchedData; //!!!
 
-      for (let item of data) {
-        const tr = createEl({el:"tr"});
-        thNames.forEach((thName) => {
-          const td = createEl({el:"td", text: item[thName], styles: `
+    for (let item of data) {
+      const tr = createEl({ el: "tr" });
+      thNames.forEach((thName) => {
+        const td = createEl({
+          el: "td",
+          text: item[thName],
+          styles: `
           padding: 10px;
-          `});
-          tr.append(td);
+          `,
         });
-        tbody.append(tr);
-      }
-      document.body.append(table);
+        tr.append(td);
+      });
+      tbody.append(tr);
+    }
+    document.body.append(table);
   };
 
   createTable(
