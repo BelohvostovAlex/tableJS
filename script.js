78dcdbdc9ac38e649e1c6a4987fcc7cfe1b8c2d9
Belakhvostau Aliaksandr <aliaksandr@wsa-089-23b.local>
2022-05-10 15:21:25 +0300

    3 table classes

--- script.js
+++ script.js
@@ -9,91 +9,185 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const deleteItem = async (config) => {
-    try {
-      const { url, id } = config;
-      const response = await fetch(`${url}/${id}`, {
-        method: "DELETE",
-      });
-      const json = await response.json();
-      return json;
-    } catch (error) {
-      throw new Error(error.message);
+  class Table {
+    constructor(tableArgs) {
+      const { tableCaption, id, thNames, fetchedData } = tableArgs;
+      this.tableCaption = tableCaption;
+      this.id = id;
+      this.thNames = thNames;
+      this.fetchedData = fetchedData;
     }
-  };
 
-  const createCheckbox = (config) => {
-    const { el, type, attribute, attributeVal } = config;
-    const input = createEl({ el });
-    input.type = type;
-    input.setAttribute(attribute, attributeVal);
-    return input;
-  };
-
-  const createEl = (config) => {
-    const { el, id, text, styles } = config;
-    const tag = document.createElement(el);
-    tag.textContent = text;
-    if (styles) {
+    createEl = (config) => {
+      const { el, id, text, styles } = config;
+      const tag = document.createElement(el);
+      tag.textContent = text;
       tag.style.cssText = styles;
-    }
-    if (id) {
       tag.setAttribute("id", id);
-    }
-    return tag;
-  };
+      return tag;
+    };
 
-  const createTable = (tableArgs) => {
-    const { tableCaption, id, thNames, fetchedData, hasInput } = tableArgs;
-    const table = createEl({
-      el: "table",
-      id: id,
-      styles: `
-        width: 1200px;
-        background: gray;
-        padding: 20px;
-        margin: 0 auto;
-        text-align: center;
-        border: 2px solid black;
-        `,
-    });
-
-    const caption = createEl({
-      el: "caption",
-      text: tableCaption,
-      styles: `
-        color: gray;
-        font-size: 30px;
-        padding: 10px 0px;
-        `,
-    });
-    const thead = createEl({ el: "thead" });
-    const trHead = createEl({ el: "tr" });
-    const tbody = createEl({ el: "tbody" });
-
-    table.append(caption);
-    table.append(thead);
-    table.append(tbody);
-    thead.append(trHead);
-
-    thNames.forEach((title) => {
-      const th = createEl({
-        el: "th",
-        text: title,
+    createTable = () => {
+      const table = this.createEl({
+        el: "table",
+        id: this.id,
         styles: `
-          font-size: 18px;
-          text-transform: uppercase;
+          width: 1200px;
+          background: gray;
+          padding: 20px;
+          margin: 0 auto;
+          text-align: center;
+          border: 2px solid black;
+          `,
+      });
+      const caption = this.createEl({
+        el: "caption",
+        text: this.tableCaption,
+        styles: `
+          color: gray;
+          font-size: 30px;
           padding: 10px 0px;
           `,
       });
-      trHead.append(th);
-    });
 
-    for (let item of fetchedData) {
-      const tr = createEl({ el: "tr" });
-      if (hasInput) {
-        const td = createEl({ el: "td", styles: `padding: 10px;` });
-        const input = createCheckbox({
+      const thead = this.createEl({ el: "thead" });
+      const trHead = this.createEl({ el: "tr" });
+      const tbody = this.createEl({ el: "tbody" });
+
+      table.append(caption);
+      table.append(thead);
+      table.append(tbody);
+      thead.append(trHead);
+
+      this.thNames.forEach((title) => {
+        const th = this.createEl({
+          el: "th",
+          text: title,
+          styles: `
+            font-size: 18px;
+            text-transform: uppercase;
+            padding: 10px 0px;
+            `,
+        });
+        trHead.append(th);
+      });
+
+      this.fetchedData.forEach((item) => {
+        const tr = this.createEl({ el: "tr" });
+        this.thNames.forEach((thName) => {
+          const td = this.createEl({
+            el: "td",
+            text: item[thName],
+            styles: `
+              padding: 10px;
+              `,
+          });
+          tr.append(td);
+        });
+        tbody.append(tr);
+      });
+      return document.body.append(table);
+    };
+  }
+
+  class TableWithDeleting extends Table {
+    constructor(tableArgs) {
+      super(tableArgs);
+    }
+    createCheckbox = (config) => {
+      const { el, type, attribute, attributeVal } = config;
+      const input = this.createEl({ el });
+      input.type = type;
+      input.setAttribute(attribute, attributeVal);
+      return input;
+    };
+
+    createBtn = (url) => {
+      const button = this.createEl({
+        el: "button",
+        text: "delete",
+        styles: `width: 60px; height: 30px; margin: 20px auto; display: block; background: gray; border: none; color: white;`,
+      });
+      const table = document.getElementById(this.id);
+      table.after(button);
+
+      button.addEventListener("click", () => this.deleteRow(url, this.id));
+    };
+
+    deleteItem = async (config) => {
+      try {
+        const { url, id } = config;
+        const response = await fetch(`${url}/${id}`, {
+          method: "DELETE",
+        });
+        const json = await response.json();
+        return json;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    };
+
+    deleteRow = (url) => {
+      const currTable = document.getElementById(this.id);
+      const allCheckboxes = currTable.querySelectorAll("input");
+      allCheckboxes.forEach((el) => {
+        if (el.checked) {
+          const id = el.getAttribute("id");
+          this.deleteItem({ url, id });
+          el.closest("tr").remove();
+        }
+      });
+    };
+
+    createTableWithDel = () => {
+      const table = this.createEl({
+        el: "table",
+        id: this.id,
+        styles: `
+          width: 1200px;
+          background: gray;
+          padding: 20px;
+          margin: 0 auto;
+          text-align: center;
+          border: 2px solid black;
+          `,
+      });
+      const caption = this.createEl({
+        el: "caption",
+        text: this.tableCaption,
+        styles: `
+          color: gray;
+          font-size: 30px;
+          padding: 10px 0px;
+          `,
+      });
+
+      const thead = this.createEl({ el: "thead" });
+      const trHead = this.createEl({ el: "tr" });
+      const tbody = this.createEl({ el: "tbody" });
+
+      table.append(caption);
+      table.append(thead);
+      table.append(tbody);
+      thead.append(trHead);
+
+      this.thNames.forEach((title) => {
+        const th = this.createEl({
+          el: "th",
+          text: title,
+          styles: `
+            font-size: 18px;
+            text-transform: uppercase;
+            padding: 10px 0px;
+            `,
+        });
+        trHead.append(th);
+      });
+
+      this.fetchedData.forEach((item) => {
+        const tr = this.createEl({ el: "tr" });
+        const td = this.createEl({ el: "td", styles: `padding: 10px;` });
+        const input = this.createCheckbox({
           el: "input",
           type: "checkbox",
           attribute: "id",
@@ -101,121 +195,157 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         td.append(input);
         tr.append(td);
-        thNames.slice(1).forEach((thName) => {
-          const TdDto = { name: thName, body: item.body, cb: unique, item };
-
-          const td = createEl({
+        this.thNames.slice(1).forEach((thName) => {
+          const td = this.createEl({
             el: "td",
-            text: createUniqTd(TdDto),
+            text: item[thName],
             styles: `
-            padding: 10px;
-            `,
+                  padding: 10px;
+                  `,
           });
           tr.append(td);
         });
-      } else {
-        thNames.forEach((thName) => {
-          const TdDto = { name: thName, body: item.body, cb: unique, item };
-
-          const td = createEl({
-            el: "td",
-            text: createUniqTd(TdDto),
-            styles: `
-            padding: 10px;
-            `,
-          });
-          tr.append(td);
-        });
-      }
-
-      tbody.append(tr);
-    }
-    document.body.prepend(table);
-  };
-
-  const createBtn = (url, tableId) => {
-    const button = createEl({
-      el: "button",
-      text: "delete",
-      styles: `width: 60px; height: 30px; margin: 20px auto; display: block; background: gray; border: none; color: white;`,
-    });
-    const table = document.getElementById(tableId);
-    table.after(button);
-
-    button.addEventListener("click", () => deleteRow(url, tableId));
-  };
-
-  const deleteRow = (url, tableId) => {
-    const currTable = document.getElementById(tableId);
-    const allCheckboxes = currTable.querySelectorAll("input");
-    allCheckboxes.forEach((el) => {
-      if (el.checked) {
-        const id = el.getAttribute("id");
-        deleteItem({ url, id });
-        el.closest("tr").remove();
-      }
-    });
-  };
+        tbody.append(tr);
+      });
+      return document.body.append(table);
+    };
+  }
 
   getData("https://jsonplaceholder.typicode.com/users")
-    .then((data) =>
-      createTable({
+    .then((data) => {
+      const table = new TableWithDeleting({
         tableCaption: "Table #1 (Users data)",
         id: "table1",
         thNames: ["check", "name", "phone", "website"],
         fetchedData: data,
-        hasInput: true,
-      })
-    )
-    .then(() =>
-      createBtn("https://jsonplaceholder.typicode.com/users", "table1")
-    )
-    .catch((err) => {
-      throw new Error(err.message);
-    });
+      });
+      return table;
+    })
+    .then((res) => {
+      res.createTableWithDel();
+      return res;
+    })
+    .then((res) => res.createBtn("https://jsonplaceholder.typicode.com/users"));
+
+  class TableWithUnique extends Table {
+    constructor(tableArgs) {
+      super(tableArgs);
+    }
+
+    createTableWithUniq() {
+      const table = this.createEl({
+        el: "table",
+        id: this.id,
+        styles: `
+              width: 1200px;
+              background: gray;
+              padding: 20px;
+              margin: 0 auto;
+              text-align: center;
+              border: 2px solid black;
+              `,
+      });
+
+      const caption = this.createEl({
+        el: "caption",
+        text: this.tableCaption,
+        styles: `
+              color: gray;
+              font-size: 30px;
+              padding: 10px 0px;
+              `,
+      });
+      const thead = this.createEl({ el: "thead" });
+      const trHead = this.createEl({ el: "tr" });
+      const tbody = this.createEl({ el: "tbody" });
+
+      table.append(caption);
+      table.append(thead);
+      table.append(tbody);
+      thead.append(trHead);
+
+      this.thNames.forEach((title) => {
+        const th = this.createEl({
+          el: "th",
+          text: title,
+          styles: `
+                font-size: 18px;
+                text-transform: uppercase;
+                padding: 10px 0px;
+                `,
+        });
+        trHead.append(th);
+      });
+
+      this.fetchedData.forEach((item) => {
+        const tr = this.createEl({ el: "tr" });
+        this.thNames.forEach((thName) => {
+          const TdDto = {
+            name: thName,
+            body: item.body,
+            cb: this.unique,
+            item,
+          };
+
+          const td = this.createEl({
+            el: "td",
+            text: this.createUniqTd(TdDto),
+            styles: `
+                  padding: 10px;
+                  `,
+          });
+          tr.append(td);
+        });
+        tbody.append(tr);
+      });
+      document.body.prepend(table);
+    }
+
+    unique = (str, isUnique) => {
+      const arr = str.split("").filter((item) => item !== " " && item !== "\n");
+      const objUniq = {};
+      for (let i = 0; i < arr.length; i++) {
+        if (objUniq[arr[i]]) {
+          objUniq[arr[i]] += 1;
+        } else {
+          objUniq[arr[i]] = 1;
+        }
+      }
+      const uniqArr = Object.values(objUniq);
+      const uniqueLetters = uniqArr.filter((item) => item < 2).length;
+      const moreThanOnce = uniqArr.filter((item) => item > 1).length;
+
+      if (isUnique) {
+        return uniqueLetters;
+      }
+      return moreThanOnce;
+    };
+
+    createUniqTd = (dto) => {
+      const { name, body, cb, item } = dto;
+      switch (name) {
+        case "unique":
+          return cb(body, true);
+        case "more than 1":
+          return cb(body, false);
+        default:
+          return item[name];
+      }
+    };
+  }
 
   getData("https://jsonplaceholder.typicode.com/posts?_limit=10")
-    .then((data) =>
-      createTable({
+    .then((data) => {
+      const table = new TableWithUnique({
         tableCaption: "Table #2 (Posts data)",
         id: "table2",
         thNames: ["id", "title", "body", "unique", "more than 1"],
         fetchedData: data,
-      })
-    )
-    .catch((err) => {
-      throw new Error(err.message);
+      });
+      return table;
+    })
+    .then((res) => {
+      res.createTableWithUniq();
+      return res;
     });
-
-  const unique = (str, isUnique) => {
-    const arr = str.split("").filter((item) => item !== " " && item !== "\n");
-    const objUniq = {};
-    for (let i = 0; i < arr.length; i++) {
-      if (objUniq[arr[i]]) {
-        objUniq[arr[i]] += 1;
-      } else {
-        objUniq[arr[i]] = 1;
-      }
-    }
-    const uniqArr = Object.values(objUniq);
-    const uniqueLetters = uniqArr.filter((item) => item < 2).length;
-    const moreThanOnce = uniqArr.filter((item) => item > 1).length;
-
-    if (isUnique) {
-      return uniqueLetters;
-    }
-    return moreThanOnce;
-  };
-
-  const createUniqTd = (dto) => {
-    const { name, body, cb, item } = dto;
-    switch (name) {
-      case "unique":
-        return cb(body, true);
-      case "more than 1":
-        return cb(body, false);
-      default:
-        return item[name];
-    }
-  };
 });
